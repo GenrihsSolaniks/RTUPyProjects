13dc02533bb23e13ac13efd2b5b9b47c0c5a2ed0
Artemijs Plakitins <plakitins@gmail.com>
2024-01-02 15:05:31 +0200

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,3 +1,7 @@
+# Nepiecišamu bibliotēku importēšana
+
+######################## Selenium bibliotēkas #######################
+
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -5,18 +9,60 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+####################### Bibliotēkas lai stradātu ar Excel failiem ######################
 
-service = Service()
-option = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=option)
+from openpyxl import Workbook, load_workbook
+import pandas as pd
 
-url = "https://translate.google.com/"
-driver.get(url)
-time.sleep(2)
+english_info = "English"
+german_info = "German"
+russian_info = "Russia"
+chinese_info = "Chinese"
+polish_info = "Polish"
+portuguese_info = "Portuguese"
 
-find = driver.find_element(By.class,"er8xn")
-find.clear()
-find.send_keys(get_info)
-find = driver.find_element(By.ID,"output")
-temp = find.get_attribute("value")
+info_list = []
+
+fail = pd.read_excel("data1.xlsx")
+df_description = pd.DataFrame(fail)
+
+if english_info in df_description['Language'].values:
+    info_list = fail.values.tolist()
+
+print(info_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#service = Service()
+#option = webdriver.ChromeOptions()
+#driver = webdriver.Chrome(service=service, options=option)
+
+#url = "https://www.deepl.com/translator"
+#driver.get(url)
+#time.sleep(2)
+
+#find = driver.find_element(By.class,"er8xn")
+#find.clear()
+#find.send_keys(get_info)
+#find = driver.find_element(By.ID,"output")
+#temp = find.get_attribute("value")
 
